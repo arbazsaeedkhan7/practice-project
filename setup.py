f611f9847e7728ce4ef14c15e08be48670adf322
Arbaaz Khan <arbazsaeedkhan7@gmail.com>
2024-06-01 13:41:19 +0500

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from typing import List
+from setuptools import find_packages, setup
+
+HYPHEN_E_DOT='-e .'
+def get_requirements(file_path: str)->List[str]:
+    '''
+    Read the requirements file and return the list of requirements.
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        [req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+setup(
+name='practice-project',
+version="0.0.1",
+author="Arbaaz",
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
